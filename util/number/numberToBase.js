06decc8b3e0d8e1bbbe9f951c48af5b7ade55b5e
= <=>
2012-01-23 23:04:12 -0600

    Updating the docs a bit.

--- util/number/numberToBase.js
+++ util/number/numberToBase.js
@@ -1,7 +1,10 @@
 define(function () {
     /**
      * Takes an arbitrary number and converts it to an Array of numbers   
-     * representing the increment of the whole at the given offset. Example, 
+     * representing the increment of the whole at the given offset. Example: 
+     * 
+     *  - f(10101110, 8, 2) --> [2, 5, 6]
+     *  - f(4, 2) --> [1, 0, 0] 
      * 
      * @param {number} number
      * @param {number} newBase
@@ -14,7 +17,7 @@ define(function () {
         // Lets us accept any base for conversion.
         number = parseInt(number, originalBase || 10);
         
-        // Using do, wince the loop must execute once;
+        // Using do, since the loop must execute once;
         do {
             // We'll find the least significant first.
             digits.unshift(number % newBase);
